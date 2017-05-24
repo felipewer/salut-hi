8342fe86a0fff9a93ad573e946e4997469ddf509
Felipe Carlos Werlang <felipewer@gmail.com>
2017-05-24 00:39:46 -0300

    App bootstraped.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+const App = () =>
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to Salut Hi!</h2>
+    </div>
+    <p className="App-intro">
+      Language classes taylored for your needs.
+    </p>
+    <center><strong>Comming soon!</strong></center>
+  </div>
+
+export default App;
